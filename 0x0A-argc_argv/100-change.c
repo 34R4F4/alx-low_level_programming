1caee8de770923fff60373de0f5f1c22a4723cdc
Arafa <khalafarafa@gmail.com>
2023-10-27 20:07:48 -0700

    0x0A.c-16

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -16,14 +16,25 @@ int main(int argc, char *argv[])
 	{
 		int i, least = 0, money = atoi(argv[1]);
 		int cents[] = {25, 10, 5, 2, 1};
-
+	for (i = 0; i < 5; i++)
+	{
 		for (money >= cents[i])
 		{
 			least += money / cents[i];
 			money = money % cents[i];
 
 			if (money % cents[i] == 0)
-			
-
+			{
+				break;
+			}
+		}
+	}
+	printf("%d\n", least);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 	return (0);
 }
