3eb70930dd089452f5f3b272e77af3d7bc4d1c1f
Arafa <khalafarafa@gmail.com>
2023-10-27 19:48:24 -0700

    0x0A.c-12

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,13 +18,8 @@ int main(int argc, char *argv[])
 	while (--argc)
 	{
 		for (c = argv[argc]; *c; c++)
-		{
 			if (*c < '0' || *c > '9')
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
+				return (printf("Error\n"), 1);
 		result += atoi(argv[argc]);
 	}
 	printf("%d\n", result);
