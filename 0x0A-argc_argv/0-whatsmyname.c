8fafff44b1cb62ae5c5c307480da611f2648d93e
Arafa <khalafarafa@gmail.com>
2023-10-27 18:57:30 -0700

    0x09-02

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * @argc : 1
+ * @argv : 2
+ *
+ * Return: return
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc-1]);
+	return0;
+}
