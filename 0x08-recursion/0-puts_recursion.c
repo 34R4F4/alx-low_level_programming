0b26e6fbf8bed70aa4e0354435a7e098f0d099cd
Arafa <khalafarafa@gmail.com>
2023-10-28 16:35:34 -0700

    0x08.c-00

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function print string using recursion
+ *
+ * @s : the string to print
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
