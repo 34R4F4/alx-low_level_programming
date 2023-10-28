ba3335f8d9773a03bcb320dc1582c8c03a5b79bd
Arafa <khalafarafa@gmail.com>
2023-10-27 19:11:25 -0700

    0x0A.c-04

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * @argc : 1
+ * @argv : 2
+ *
+ * Return: return
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i <= argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
