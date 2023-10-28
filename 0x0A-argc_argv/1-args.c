faf7dd9c3b3dc2992a717a2648d1b1f0e1b234c9
Arafa <khalafarafa@gmail.com>
2023-10-27 19:05:55 -0700

    0x0A.c-03

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * @argc : 1
+ * @argv : 2
+ *
+ * Return: return
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
