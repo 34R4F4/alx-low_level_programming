9d333d0a5371b50375c243749f7f37ad5b1ae5c6
Arafa <khalafarafa@gmail.com>
2023-10-28 20:23:45 -0700

    0x08.c-04

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,13 +10,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	while (*s != '\0')
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		_puts_recursion(s + 1);
+		_print_rev_recursion(s + 1);
 		 _putchar(*s);
 	}
 }
