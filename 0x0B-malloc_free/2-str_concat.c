0194cf2239a5b37c270e0dd5d0277b7ce6e349e0
Arafa <khalafarafa@gmail.com>
2023-10-31 13:15:40 -0700

    0x08.c-11

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,62 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - function
+ *
+ * @str: 1
+ *
+ * Return: return
+ */
+
+int len(char *str);
+
+char *str_concat(char *s1, char *s2)
+{
+	int l1 = len(s1);
+	int l2 = len(s2);
+	int i = 0;
+	char *text;
+
+	if (s1 = NULL)
+		s1 = '\0';
+	if (s2 = NULL)
+                s2 = '\0';
+
+	text = malloc(l * sizeof(char) + 1);
+	
+	if (text == 0)
+		return (0);
+
+	for (i =0; i <= l; i++)
+	{
+		if (i < l1)
+			text[i] = s1[i];
+		else
+			text[i] = s2[i - l1];
+	}
+
+	text[i] = '\0';
+
+	return (text);
+	}
+}
+
+/**
+ * len - f
+ * @str: 1
+ * Return: r
+ */
+
+int len(char *str)
+{
+	int l = 0;
+
+	while (*str)
+	{
+		str++;
+		l++;
+	}
+	l--;
+	return (l);
+}
