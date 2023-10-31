8e31ee6f34807af7397d4c1ac83a2ec4e4922751
Arafa <khalafarafa@gmail.com>
2023-10-31 10:57:10 -0700

    0x0B-04

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *
+ * is_prime_number - check the semitricity of string (ex: level, setp on no pets)
+ *
+ * @s : checked string
+ *
+ * Return: 1 if a string is a palindrome and 0 if not.
+ *
+ */
+
+int is_palindrome(char *s)
+{
+	int l;
+
+	while (*s)
+	{
+		l++;
+		s++;
+	}
+
+
+}
