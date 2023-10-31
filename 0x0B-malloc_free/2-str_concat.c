0023718418249177d527741f72926a9883bdbdf9
Arafa <khalafarafa@gmail.com>
2023-10-31 13:51:34 -0700

    0x08.c-15

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,6 +10,7 @@
  */
 
 int len(char *str);
+void emps(char *dst, char *src);
 
 char *str_concat(char *s1, char *s2)
 {
@@ -20,9 +21,17 @@ char *str_concat(char *s1, char *s2)
 	char *text;
 
 	if (s1 == NULL)
-		s1 = '\0';
+	{
+		text = malloc(l2 * sizeof(char));
+		emps(text, s2);
+		return (text);
+	}
 	if (s2 == NULL)
-		s2 = '\0';
+	{
+		text = malloc(l2 * sizeof(char));
+		emps(text, s2);
+		return (text);
+	}
 
 	text = malloc(l * sizeof(char) + 3);
 
@@ -59,4 +68,23 @@ int len(char *str)
 	}
 	l--;
 	return (l);
+}
+
+/**
+ * emps - f
+ * @dst: 1
+ * @src: 2
+ * Return: r
+ */
+
+void emps(char *dst, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
 }
