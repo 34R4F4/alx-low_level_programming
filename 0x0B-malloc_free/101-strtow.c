6f2de0dce47f301acaf85626385fa438742db7d5
Arafa <khalafarafa@gmail.com>
2023-11-01 15:04:05 -0700

    0xB-38

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * strtow - function
+ * @str: 1
+ * Return: return
+ */
+char **strtow(char *str)
+{
+	if (str != NULL || str != "")
+	{
+
+	}
+	else
+	{
+		return (NULL);
+	}
+}
