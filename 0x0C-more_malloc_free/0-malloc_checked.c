a53e73ad157949300bd26a075df7bbccc2a8d4cb
Arafa <khalafarafa@gmail.com>
2023-11-02 20:08:16 +0200

    0x0C-01

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function
+ * @b: 1
+ * Return: return
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *m;
+
+	m = malloc(b);
+
+	if (m > 0)
+	{
+		return(m);
+	}
+	else{
+		exit(98);
+	}	
+}
