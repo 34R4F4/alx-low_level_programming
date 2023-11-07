02aefe961946cc3db5c9bf54823a34a41cd45e34
Arafa <khalafarafa@gmail.com>
2023-11-06 18:03:52 -0800

    0x0E-13

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,39 @@
 #include <stdio.h>
 
 /**
+ * len - f
+ * @s: s
+ * Return: l
+ */
+int len(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+
+/**
+ * scpy - f
+ * @s: s
+ * Return: c
+ */
+void scpy(char *x, char *y)
+{
+	int i;
+	int l = len(x);
+
+	if (x !== NULL)
+	{
+	y = malloc((sizeof(x)):
+	for (i = 0; i <= l; i++)
+
+}
+
+
+/**
  * new_dog - create dog
  * @name: name
  * @age: age
@@ -11,10 +44,12 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-if (d)
-{
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-}
+	dog_t *dt;
+
+	if (name == NULL || owner == NULL || age < 0.0)
+		return (NULL);
+
+	dt->name = name;
+	dt->age = age;
+	dt->owner = owner
 }
