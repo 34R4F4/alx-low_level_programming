939c6465646a218ce93b3bbcbc4774b2d0a06e90
Arafa <khalafarafa@gmail.com>
2023-11-06 17:46:08 -0800

    0x0E-12

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * new_dog - create dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: dog_t
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+if (d)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
+}
