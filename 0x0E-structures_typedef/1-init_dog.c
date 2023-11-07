2963c704ffcfe9a141263bbb809c5e2cd6d7cfad
Arafa <khalafarafa@gmail.com>
2023-11-06 16:57:27 -0800

    0x0E-02

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+/**
+ * init_dog - create dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = *name;
+	d.age = age;
+	d.owner = *owner;
+}
