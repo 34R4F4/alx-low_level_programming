d01309cdc7eb4d40cecd8480d60e5a7d9fd6dbca
Arafa <khalafarafa@gmail.com>
2023-11-16 06:32:59 -0800

    0x12-01

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strl - f
+ * @str: 1
+ * Return: l
+ */
+int _strl(char *str)
+{
+	int l = 0;
+
+	if (!str)
+		return (0);
+	while (*str++)
+		l++;
+	return (l);
+}
+
+/**
+ * print_list - f
+ * @h: 1
+ * Return: t
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strl(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		n++;
+	}
+
+	return (n);
+}
