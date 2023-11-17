c45def9249351bfef0391fdebb3be12b3c252a6e
Arafa <khalafarafa@gmail.com>
2023-11-17 15:36:12 -0800

    0x12-11

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#iclude "lists.h"
+
+void _constructor(void) __attribute__ ((constructor))
+/**
+ * _constructor - execute before main
+ *
+ * Return: v
+ */
+
+void _constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
