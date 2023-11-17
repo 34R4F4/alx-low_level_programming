c9d8a503168764f00b52ecc317ce5f61c01b791c
Arafa <khalafarafa@gmail.com>
2023-11-17 15:51:54 -0800

    0x12-16

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,6 +9,6 @@ void print_constructor(void) __attribute__((constructor));
 
 void print_constructor(void)
 {
-	printf("You're beat! and yet, you must allow,\n);
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
