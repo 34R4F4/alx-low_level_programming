9e48aed96382573c8f235055608d9babf50d72b8
Arafa <khalafarafa@gmail.com>
2023-11-17 15:41:13 -0800

    0x12-12

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #iclude "lists.h"
 
-void _constructor(void) __attribute__ ((constructor))
+void print_constructor(void) __attribute__((constructor))
 /**
- * _constructor - execute before main
+ * print_constructor - execute before main
  *
  * Return: v
  */
 
-void _constructor(void)
+void print_constructor(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
