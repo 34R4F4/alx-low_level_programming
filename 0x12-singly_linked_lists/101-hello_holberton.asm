e4ed5af2eeb1cc5ac2627427e896d77a31f49214
Arafa <khalafarafa@gmail.com>
2023-11-17 16:13:28 -0800

    018

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,11 @@
-section .data
-    hello db 'Hello, Holberton', 0  ; Null-terminated string
+section		.data
+	 hello db 'Hello, Holberton', 0xa, 0
 
-section .text
-    global main
-
-    extern printf
-    extern exit
+section		.text
+	global main
+	extern printf
 
 main:
-    mov rdi, hello        ; Format string
-    call printf           ; Call printf function
-    mov rdi, 0            ; Exit code 0
-    call exit             ; Call exit function
-
+	mov	edi, hello
+	mov	eax, 0
+	call	printf
