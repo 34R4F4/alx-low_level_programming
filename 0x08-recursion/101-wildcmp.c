4f3d390f263aba3bd0b59b84b6276a5412c4dfad
Arafa <khalafarafa@gmail.com>
2023-11-19 14:23:12 -0800

    0x08-01

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * wildcmp - f
+ * @s1: 1
+ * @s2: 2
+ * Return: 0/1
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+
+}
--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -12,8 +12,13 @@ int _strlen_recursion(char *s)
 {
 	int l = 0;
 
+	if (!s)
+		return (l);
+
 	if (*s != '\0')
 	{
 		l += _strlen_recursion(s + 1) + 1;
 	}
+
+	return (l);
 }
