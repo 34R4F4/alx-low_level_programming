e565c7bfa9339f601f41f10ff5501549dd6396f1
Arafa <khalafarafa@gmail.com>
2023-11-20 15:35:21 -0800

    0x13-12

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,13 +14,15 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (!new_node)
 		return (NULL);
 
-	new_node = *head;
-
-	while (new_node)
-		new_node->next;
-
 	new_node->n = n;
 	new_node->next = NULL;
+
+	while (head)
+	{
+		*head = (*head)->next;
+	}
+
+	*head = new_node;
 
 	return (new_node);
 }
