ecf56413a62ddb124deb15747179a35c1c086fe3
Arafa <khalafarafa@gmail.com>
2023-11-21 15:55:57 -0800

    0x13-29

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - f
+ * @head: 1
+ * @index: 2
+ * Return: nth
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *nth_node;
+	unsigned int i;
+
+	for (i = 0; i <= index; i++)
+	{
+		if (!head)
+			return (NULL);
+
+		head = head->next;
+	}
+
+	nth = *head;
+	return (nth);
+}
