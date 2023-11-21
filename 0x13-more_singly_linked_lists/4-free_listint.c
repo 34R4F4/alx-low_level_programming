46f05467c21acb58c8eef608977fbae4d6eb84c0
Arafa <khalafarafa@gmail.com>
2023-11-21 14:50:03 -0800

    0x13-19

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,10 +6,12 @@
  * Return: void
  */
 
-void free_listint(listint_t *head) {
+void free_listint(listint_t *head)
+{
 	while (head)
 	{
 	listint_t *temp_node = head;
+
 	head = head->next;
 	free(temp_node);
 	}
