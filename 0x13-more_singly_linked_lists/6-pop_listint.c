342680404c4045b32b520e61733b09a3774d1be7
Arafa <khalafarafa@gmail.com>
2023-11-21 15:28:29 -0800

    0x13-24

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * pop_listint - f
+ * @head: 1
+ * Return: void
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *new_h;
+	int the_n;
+
+	if (!head)
+		return;
+
+	new_h = (*head)->next;
+	the_n = (*head)->n;
+
+	free(temp_node);
+
+	*head = new_h;
+
+	return (the_n);
+}
