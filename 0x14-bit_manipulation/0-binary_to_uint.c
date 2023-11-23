2c9e95e6c7d209601fdda958f039f66d3e865a76
Arafa <khalafarafa@gmail.com>
2023-11-23 15:51:07 -0800

    0x14-01

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,18 +1,5 @@
 #include "main.h"
 
-/**
- * power2 - f
- * @n: 1
- * Return: p
- */
-unsigned int power2(unsigned int n)
-{
-	if (n == 0)
-		return (1);
-	else
-		return (2 * power2(n - 1));
-}
-
 /*
  * binary_to_uint - f
  * @b: 1
@@ -21,19 +8,18 @@ unsigned int power2(unsigned int n)
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int n = 0;
-	unsigned int i = 0;
+	int i = 0;
 
 	if  (!b)
 		return (0);
 
-	while (*b)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (*b != '0' || *b != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-
-		n += (2 * power2(i)) * (*b);
-		i++;
-		b++;
+		n <<= 1;
+		if (b[i] == '1')
+			n |= 1;
 	}
 	return (n);
 }
