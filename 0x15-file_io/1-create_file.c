27a697b4c7d2b4d4ab5ddf3e088c8a0abe93cbe6
Arafa <khalafarafa@gmail.com>
2023-11-27 18:35:55 -0800

    0x15-03

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * len - f
+ * @s: 1
+ * Return: len
+ */
+int len(char *s)
+{
+	int l = 0;
+
+	while (*s++)
+		i++;
+
+	return (l);
+}
+
+/**
+ * create_file - f
+ * @create_file: 1
+ * @text_content: 2
+ * Return: 1/-1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fo;
+	ssize_t text = 0;
+	ssize_t len = len(text_content);
+
+	if (!filename)
+		return (-1);
+
+	fo = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fo == -1)
+		return (-1);
+
+	if (len)
+		text = write(fo, text_content, len);
+
+	close(fo);
+	return (text == len ? 1 : -1);
+}
