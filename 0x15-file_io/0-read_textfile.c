dfff195c4e9609916b565de0b34b9d43b625f51f
Arafa <khalafarafa@gmail.com>
2023-11-27 18:08:24 -0800

    0x15-00

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * read_textfile - f
+ * @filename: 1
+ * @letters: 2
+ * Return: t
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int f_state;
+	char buf[R_BUF * 8];
+	ssize_t re_turn;
+
+	if (filename == NULL)
+		return (0);
+	if(!filename || !letters)
+		return (0);
+
+	f_state = open(filename, 0_RDONLY);
+	if (f_state == -1)
+		return (0);
+
+	re_turn = read(f_state, &buf[0], letters);
+	re_turn = write(STDOUT_FILENO, &buf[0], re_turn);
+
+	close(f_state);
+	return (re_turn);
+}
