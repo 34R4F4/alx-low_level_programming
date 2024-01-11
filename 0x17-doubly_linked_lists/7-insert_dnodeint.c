91f9c8d49727026b8f2f559f54240de32bd73fb3
Arafa <khalafarafa@gmail.com>
2024-01-11 13:31:40 -0800

    0x17-13

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,51 +1,51 @@
 #include "lists.h"
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position.
- * @h: linked-list HEAD.
- * @idx: the node index in the linked list, starting from 0.
- * @n: the node->n vlue.
- * Return: the address of the new node, or NULL if it failed.
+ * insert_dnodeint_at_index - Inserts a new node at a given position
+ * @h: Pointer to the head of the doubly linked list
+ * @idx: Index where the new node should be added (starting at 0)
+ * @n: Value to be stored in the new node
+ * Return: Address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node;
-	dlistint_t *temp = *h;
-	dlistint_t *current = *h;
-	unsigned int l = 0;
+dlistint_t *new_node, *temp = *h;
+unsigned int count = 0;
 
-	while (temp != NULL)
-	{
-		temp = temp->next;
-		l++;
-	}
-	if (idx > l + 1)
+while (temp != NULL)/* Count the number of nodes in the list */
+{
+	temp = temp->next;
+	count++;
+}
+if (idx > count)/* Check if the given index is valid */
+	return (NULL);
+if (idx == 0)/* If index is 0, add node at the beginning */
+	return (add_dnodeint(h, n));
+if (idx == count)/* If index is the number of nodes, add node at the end */
+	return (add_dnodeint_end(h, n));
+/* Traverse to find the node at the position before the insertion point */
+temp = *h;
+for (count = 0; count < idx - 1; count++)
+{
+	if (temp == NULL)
 		return (NULL);
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-	if (idx == l + 1)
-		return (add_dnodeint_end(h, n));
-
+	temp = temp->next;
+}
+/* Check if the current node is not NULL (index is valid) */
+if (temp != NULL)
+{
+	/* Allocate memory for the new node */
 	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
+	if (new_node == NULL)
 		return (NULL);
+	/* Initialize the data of the new node */
 	new_node->n = n;
-
-	while (current != NULL && idx > 0)
-	{
-		current = current->next;
-		idx -= 1;
-	}
-
-	if (current != NULL && idx == 0)
-	{
-		new_node->prev = current->prev;
-		current->prev->next = new_node;
-		new_node->next = current;
-		return (new_node);
-	}
-	else
-	{
-		free(new_node);
-		return (NULL);
-	}
+	new_node->prev = temp;
+	new_node->next = temp->next;
+	/* Update pointers to insert the new node at the specified index */
+	if (temp->next != NULL)
+		temp->next->prev = new_node;
+	temp->next = new_node;
+	return (new_node);/* Return the address of the new element */
+}
+return (NULL); /* Index is out of bounds */
 }
