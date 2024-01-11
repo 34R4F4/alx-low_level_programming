d531d2ef6a71f3d7ebeb5a0e0b6c078b5b8b8a03
Arafa <khalafarafa@gmail.com>
2024-01-11 10:06:10 -0800

    0x17-02

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * dlistint_len - function returns the number of elements in a linked dlistint_t list.
+ * @h: head pointer
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t r = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		r++;
+	}
+
+	return (r);
+}
